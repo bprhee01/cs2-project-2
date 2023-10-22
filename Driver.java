3618adfac1c9d125b8efac882ace9a2ac0ab2449
benrhee <thebenrhee@gmail.com>
2023-10-22 18:19:31 -0500

    Driver class

--- Driver.java
+++ Driver.java
@@ -1,3 +1,41 @@
-public class Driver {
-    
+public class Driver implements Comparable<Driver> {
+    private String name;
+    private double area;
+    public static String comparisonVar = "name"; 
+
+    // Constructors
+    public Driver() {}
+    public Driver(String name){
+        this.name = name;
+    }
+
+    // Accessors
+    public String getName() {
+        return name;
+    }
+    public double getArea(){
+        return area;
+    }
+
+    // Mutators
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setArea(double area){
+        this.area = area;
+    }
+
+    // CompareTo 
+    public int compareTo(Driver other) {
+        if (comparisonVar.equals("name")) {
+            return this.name.compareTo(other.name);
+        } else {
+            return Double.compare(this.area, other.area);
+        }
+    }
+
+    // toString method
+    public String toString(){
+        return name + "\t" + area;
+    }
 }
